dee1e762b8a95b143dce82611650c022165a6d5b
Alex Shimanovich <shimanov@post.bgu.ac.il>
2016-11-29 17:34:14 +0200

    alex commit

--- code/fusion/src/main/java/il/ac/bgu/fusion/Main.java
+++ code/fusion/src/main/java/il/ac/bgu/fusion/Main.java
@@ -3,15 +3,35 @@ package il.ac.bgu.fusion;
 import com.google.gson.Gson;
 import il.ac.bgu.fusion.classes.Elipse;
 
+import java.io.FileWriter;
+import java.io.IOException;
 /**
  * Created by Guy Yafe.
  */
 public class Main {
 
-  public static void main(String ... args){
+
+
+    public static void main(String ... args)throws IOException{
     Gson gson = new Gson();
     Elipse el= new Elipse(124, 4, 45, 4, 1, 2, 3);
-    System.out.println(gson.toJson(el));
+        String json = gson.toJson(el);
+        try (FileWriter writer = new FileWriter("fuse.json")) {
+
+            gson.toJson(el, writer);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+      //JsonObject obj = new JsonObject();
+        //gson.toJson(el, new FileWriter("~/Documents/jsonfile"));
+
+
+
+    }
+
   }
 
-}
+
