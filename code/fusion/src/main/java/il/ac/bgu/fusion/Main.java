7cc0fe7bf2f552ac8134c1a1e509f22ba52bcbd7
Stas Rodov <stasr88@gmail.com>
2016-11-29 14:09:37 +0200

    Stas commit

--- code/fusion/src/main/java/il/ac/bgu/fusion/Main.java
+++ code/fusion/src/main/java/il/ac/bgu/fusion/Main.java
@@ -1,16 +1,17 @@
 package il.ac.bgu.fusion;
 
+import com.google.gson.Gson;
+import il.ac.bgu.fusion.classes.Elipse;
+
 /**
  * Created by Guy Yafe.
  */
 public class Main {
 
-public final static String HELLO ="Hello World";
   public static void main(String ... args){
-    System.out.println(printHello());
+    Gson gson = new Gson();
+    Elipse el= new Elipse(124, 4, 45, 4, 1, 2, 3);
+    System.out.println(gson.toJson(el));
   }
 
-  private static String printHello(){
-    return "HELLO";
-  }
 }
---
+++ code/fusion/src/main/java/il/ac/bgu/fusion/util/JsonReaderWriter.java
@@ -0,0 +1,11 @@
+package il.ac.bgu.fusion.util;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by Stas on 29/11/2016.
+ */
+public class JsonReaderWriter {
+
+    Gson gson = new Gson();
+}
