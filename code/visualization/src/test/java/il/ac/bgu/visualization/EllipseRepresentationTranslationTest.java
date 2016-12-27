f9e98c2bb478c18c0314b955f3375fe67108f11b
Mali Yakobov <mazyakobov@gmail.com>
2016-12-27 12:22:02 +0200

    Merge branch 'master' of https://github.com/Mali-yakobov/fusion-project
    # Conflicts:
    #	code/build.gradle
    #	code/visualization/src/main/java/il/ac/bgu/visualization/AddEllipseBox.java

--- code/visualization/src/test/java/il/ac/bgu/visualization/EllipseRepresentationTranslationTest.java
+++ code/visualization/src/test/java/il/ac/bgu/visualization/EllipseRepresentationTranslationTest.java
@@ -18,14 +18,14 @@ public class EllipseRepresentationTranslationTest {
     @Before
     public void setUp() throws Exception {
 
-        Ellipse ellipse= new Ellipse(170,400,50,20);
-        ellipse.setRotate(45);
+       // Ellipse ellipse= new Ellipse(170,400,50,20);
+        //ellipse.setRotate(45);
 
-        CovarianceEllipse covarianceEllipseFromEllipse= EllipseRepresentationTranslation.fromVizualToCovariance(ellipse);
-        Ellipse ellipseFromCovarianceEllipse= EllipseRepresentationTranslation.fromCovarianceToVizual(covarianceEllipseFromEllipse);
-        System.out.println(ellipse + " " + ellipse.getRotate());
-        System.out.println(covarianceEllipseFromEllipse);
-        System.out.println(ellipseFromCovarianceEllipse  + " " + ellipseFromCovarianceEllipse.getRotate());
+       // CovarianceEllipse covarianceEllipseFromEllipse= EllipseRepresentationTranslation.fromVizualToCovariance(ellipse);
+       // Ellipse ellipseFromCovarianceEllipse= EllipseRepresentationTranslation.fromCovarianceToVizual(covarianceEllipseFromEllipse);
+       // System.out.println(ellipse + " " + ellipse.getRotate());
+        //System.out.println(covarianceEllipseFromEllipse);
+       // System.out.println(ellipseFromCovarianceEllipse  + " " + ellipseFromCovarianceEllipse.getRotate());
         //Ellipse visFromCovFromFile= EllipseRepresentationTranslation.fromCovarianceToVizual(fromFileEllipse);
         //System.out.println(visFromCovFromFile  + " " + visFromCovFromFile.getRotate());
     }
@@ -37,18 +37,31 @@ public class EllipseRepresentationTranslationTest {
 
     @Test
     public void fromCovarianceToVizual() throws Exception {
+        CovarianceEllipse covarianceEllipse=new CovarianceEllipse(0,0,400.0,300.0,1450.0,1449.0,1050.0);
+        Ellipse ellipseFromCovarianceEllipse= EllipseRepresentationTranslation.fromCovarianceToVizual(covarianceEllipse);
+        CovarianceEllipse covarianceEllipseFromEllipse=EllipseRepresentationTranslation.fromVizualToCovariance(ellipseFromCovarianceEllipse);
+        assertEquals("equal",covarianceEllipse.getCentreX(),covarianceEllipseFromEllipse.getCentreX());
+        assertEquals("equal",covarianceEllipse.getCentreY(),covarianceEllipseFromEllipse.getCentreY());
+        assertEquals("equal",covarianceEllipse.getSx2(),covarianceEllipseFromEllipse.getSx2());
+        assertEquals("equal",covarianceEllipse.getSy2(),covarianceEllipseFromEllipse.getSy2());
+        assertEquals("equal",covarianceEllipse.getSxy(),covarianceEllipseFromEllipse.getSxy());
+        assertEquals("equal",covarianceEllipse.getId(),covarianceEllipseFromEllipse.getId());
+        assertEquals("equal",covarianceEllipse.getTimeStamp(),covarianceEllipseFromEllipse.getTimeStamp());
+
+
+    }
+
+    @Test
+    public void fromVizualToCovariance() throws Exception {
         Ellipse ellipse= new Ellipse(170,400,50,20);
         ellipse.setRotate(45);
         CovarianceEllipse covarianceEllipseFromEllipse= EllipseRepresentationTranslation.fromVizualToCovariance(ellipse);
         Ellipse ellipseFromCovarianceEllipse= EllipseRepresentationTranslation.fromCovarianceToVizual(covarianceEllipseFromEllipse);
         assertEquals("equal",ellipse.getCenterX(),ellipseFromCovarianceEllipse.getCenterX());
+        assertEquals("equal",ellipse.getCenterY(),ellipseFromCovarianceEllipse.getCenterY());
+        assertEquals("equal",ellipse.getRadiusX(),ellipseFromCovarianceEllipse.getRadiusX());
+        assertEquals("equal",ellipse.getRadiusY(),ellipseFromCovarianceEllipse.getRadiusY());
         assertEquals("equal",ellipse.getRotate(),ellipseFromCovarianceEllipse.getRotate());
-    }
-
-    @Test
-    public void fromVizualToCovariance() throws Exception {
-
-
     }
 
 }
