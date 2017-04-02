ba3b2b4dcabdc6ccd0ab9d67e168aa650c509aa3
Mali Yakobov <mazyakobov@gmail.com>
2017-04-02 23:21:00 +0300

    add commit

--- code/fusion/src/main/java/il/ac/bgu/fusion/Main.java
+++ code/fusion/src/main/java/il/ac/bgu/fusion/Main.java
@@ -2,8 +2,11 @@ package il.ac.bgu.fusion;
 
 
 
+import com.google.gson.Gson;
 import il.ac.bgu.fusion.objects.*;
+import il.ac.bgu.fusion.util.JsonReaderWriter;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -11,6 +14,7 @@ import java.util.ArrayList;
  * Created by Guy Yafe.
  */
 public class Main {
+
 
 
 
@@ -29,10 +33,17 @@ public class Main {
         cL1.add(c1);
         cL1.add(c2);
         cL1.add(c3);
-
+        ArrayList<CovarianceEllipse> cL2=new ArrayList<CovarianceEllipse>();
+        cL2.add(c4);
+        cL2.add(c5);
+        cL2.add(c6);
+        ArrayList<CovarianceEllipse> cL3=new ArrayList<CovarianceEllipse>();
+        cL3.add(c7);
+        cL3.add(c8);
+        cL3.add(c9);
         State s1=new State(cL1);
-        State s2=new State(cL1);
-        State s3=new State(cL1);
+        State s2=new State(cL2);
+        State s3=new State(cL3);
         ArrayList<State> sL1=new ArrayList<State>();
         sL1.add(s1);
         sL1.add(s2);
@@ -40,7 +51,18 @@ public class Main {
         Track t1=new Track(123,10,12,sL1);
         ArrayList<Track> tL1=new ArrayList<Track>();
         tL1.add(t1);
-        PointInTime p=new PointInTime(10,tL1);
+        PointInTime p1=new PointInTime(10,tL1);
+
+        ArrayList<PointInTime> listPointInTime=new ArrayList<PointInTime>();
+        listPointInTime.add(p1);
+        String filename= "testJson";
+        JsonReaderWriter.objectToJsonFile(listPointInTime,filename);
+        
+        String fileaddress= "D:\\sigmabit\\fusion-project\\";
+        String filepath= fileaddress + filename + ".json";
+        ArrayList<PointInTime> fromFileEllipse= JsonReaderWriter.jsonToObject(filepath);
+        System.out.print(p1.toString().equals(fromFileEllipse.get(0).toString()));
+
 
 
 
--- code/fusion/src/main/java/il/ac/bgu/fusion/util/JsonReaderWriter.java
+++ code/fusion/src/main/java/il/ac/bgu/fusion/util/JsonReaderWriter.java
@@ -4,6 +4,7 @@ package il.ac.bgu.fusion.util;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import il.ac.bgu.fusion.objects.CovarianceEllipse;
+import il.ac.bgu.fusion.objects.PointInTime;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -37,5 +38,25 @@ public class JsonReaderWriter {
      }
      return ans;
  }
+    public static void objectToJsonFile(ArrayList<PointInTime> PointInTimeObjects, String filename){
+        Gson gson = new Gson();
+        try (FileWriter writer = new FileWriter(filename + ".json")) {
+            gson.toJson(PointInTimeObjects, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<PointInTime> jsonToObject(String filepath){
+        Gson gson = new Gson();
+        ArrayList<PointInTime> ans=null;
+        Type ListType = new TypeToken<ArrayList<PointInTime>>(){}.getType();
+        try {
+            ans = gson.fromJson(new FileReader(filepath), ListType);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return ans;
+    }
 
 }
